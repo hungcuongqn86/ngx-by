1bdfa22e6ed82c67de493a5b5421a726cd75653a
hungcuongqn86@gmail.com <hungcuongqn86@gmail.com>
2019-03-16 21:15:01 +0700

    start

--- src/app/layout/_nav.ts
+++ src/app/layout/_nav.ts
@@ -4,41 +4,91 @@ export const navItems = [
         url: '/dashboard',
         icon: 'icon-speedometer'
     },
-    /*{
-        name: 'Version',
-        url: '/version',
-        icon: 'icon-star'
-    },*/
     {
-        name: 'Chủ thú',
-        url: '/mowner/owner',
-        icon: 'cui-people'
+        name: 'Giỏ hàng',
+        url: '/cart',
+        icon: 'fa fa-cart-plus'
     },
     {
-        name: 'Quản lý thú',
-        url: '/mpet',
-        icon: 'icon-puzzle',
+        name: 'Đơn hàng',
+        url: '/order',
+        icon: 'fa fa-gavel',
         children: [
             {
-                name: 'Thú',
-                url: '/mpet/pet',
-                icon: 'icon-puzzle'
+                name: 'Tất cả',
+                url: '/order/pet',
+                icon: 'fa fa-folder'
             },
             {
-                name: 'Loài',
-                url: '/mpet/species',
-                icon: 'cui-graph'
+                name: 'Chờ báo giá',
+                url: '/order/species',
+                icon: 'fa fa-folder'
             },
             {
-                name: 'Giống',
-                url: '/mpet/breed',
-                icon: 'fa fa-sitemap'
+                name: 'Chờ đặt cọc',
+                url: '/order/breed',
+                icon: 'fa fa-folder'
+            },
+            {
+                name: 'Đang mua hàng',
+                url: '/order/breed',
+                icon: 'fa fa-folder'
+            },
+            {
+                name: 'Đã mua hàng',
+                url: '/order/breed',
+                icon: 'fa fa-folder'
+            },
+            {
+                name: 'Người bán giao',
+                url: '/order/breed',
+                icon: 'fa fa-folder'
+            },
+            {
+                name: 'Kho THQC nhận',
+                url: '/order/breed',
+                icon: 'fa fa-folder'
+            },
+            {
+                name: 'Trên đường về VN',
+                url: '/order/breed',
+                icon: 'fa fa-folder'
+            },
+            {
+                name: 'Trong kho VN',
+                url: '/order/breed',
+                icon: 'fa fa-folder'
+            },
+            {
+                name: 'Đang giao hàng',
+                url: '/order/breed',
+                icon: 'fa fa-folder'
+            },
+            {
+                name: 'Đã trả hàng',
+                url: '/order/breed',
+                icon: 'fa fa-folder'
+            },
+            {
+                name: 'Thanh lý',
+                url: '/order/breed',
+                icon: 'fa fa-folder'
+            },
+            {
+                name: 'Đơn hủy',
+                url: '/order/breed',
+                icon: 'fa fa-folder'
             }
         ]
     },
-    /*{
-        name: 'Settings',
+    {
+        name: 'Kiện hàng',
         url: '/#',
-        icon: 'icon-settings',
-    }*/
+        icon: 'fa fa-cubes',
+    },
+    {
+        name: 'Ví điện tử',
+        url: '/#',
+        icon: 'fa fa-money',
+    }
 ];
