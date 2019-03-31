e16e33ea40e4a9ea0ed43dbbea4446b395ab35b7
hungcuongqn86@gmail.com <hungcuongqn86@gmail.com>
2019-04-01 00:29:15 +0700

    WalletModule

--- src/app/modules/wallet/wallet.component.ts
+++ src/app/modules/wallet/wallet.component.ts
@@ -1,8 +1,7 @@
 import {Component} from '@angular/core';
 import {UserService} from '../../services/muser/user.service';
-import {BsModalService} from 'ngx-bootstrap/modal';
-import {BsModalRef} from 'ngx-bootstrap/modal/bs-modal-ref.service';
-import {TransactionType, Transaction} from '../../models/Transaction';
+import {AuthService} from '../../auth.service';
+import {Transaction} from '../../models/Transaction';
 
 @Component({
     selector: 'app-wallet',
@@ -11,42 +10,16 @@ import {TransactionType, Transaction} from '../../models/Transaction';
 })
 
 export class WalletComponent {
-    modalRef: BsModalRef;
-    types: TransactionType[];
     transactions: Transaction[];
 
-    constructor(public userService: UserService, private modalService: BsModalService) {
-        this.getTypes();
+    constructor(public userService: UserService, public authService: AuthService) {
         this.getTransactions();
     }
 
     private getTransactions() {
-        this.userService.getTransactions(this.userService.user.id)
+        this.userService.getTransactions(this.authService.user.id)
             .subscribe(transactions => {
                 this.transactions = transactions.data.data;
             });
-    }
-
-    private getTypes() {
-        this.userService.getTransactionTypes()
-            .subscribe(types => {
-                this.types = types.data;
-            });
-    }
-
-    public confirm(): void {
-        this.userService.addTransaction()
-            .subscribe(transaction => {
-                this.modalRef.hide();
-                this.getTransactions();
-            });
-    }
-
-    public decline(): void {
-        this.modalRef.hide();
-    }
-
-    public addTransaction(template) {
-        this.modalRef = this.modalService.show(template, {class: 'modal-lg', ignoreBackdropClick: true});
     }
 }
