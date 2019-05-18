d61082fe983783e719c0b98b516eb5e21e74ab2f
hungcuongqn86@gmail.com <hungcuongqn86@gmail.com>
2019-05-18 07:42:14 +0700

    tab

--- src/app/modules/order/order/order.component.ts
+++ src/app/modules/order/order/order.component.ts
@@ -1,8 +1,9 @@
-import {Component, OnInit, ViewEncapsulation, TemplateRef} from '@angular/core';
+import {Component, OnInit, ViewEncapsulation, TemplateRef, OnDestroy} from '@angular/core';
 import {Router} from '@angular/router';
 import {Order, OrderCreate, OrderService, OrderStatus} from '../../../services/order/order.service';
 import {BsModalService} from 'ngx-bootstrap/modal';
 import {BsModalRef} from 'ngx-bootstrap/modal/bs-modal-ref.service';
+import {Subscription} from 'rxjs';
 
 @Component({
     selector: 'app-order',
@@ -11,13 +12,14 @@ import {BsModalRef} from 'ngx-bootstrap/modal/bs-modal-ref.service';
     encapsulation: ViewEncapsulation.None
 })
 
-export class OrderComponent implements OnInit {
+export class OrderComponent implements OnInit, OnDestroy {
     order: OrderCreate;
     orders: Order[];
     status: OrderStatus[] = [];
     totalItems = 0;
     modalRef: BsModalRef;
     errorMessage: string[] = [];
+    sub: Subscription;
 
     inputBaoGia = {id: null, content: null};
 
@@ -43,7 +45,10 @@ export class OrderComponent implements OnInit {
 
     public searchOrders() {
         this.orderService.showLoading(true);
-        this.orderService.getOrders()
+        if (this.sub) {
+            this.sub.unsubscribe();
+        }
+        this.sub = this.orderService.getOrders()
             .subscribe(orders => {
                 this.orders = orders.data.data;
                 this.totalItems = orders.data.total;
@@ -77,5 +82,16 @@ export class OrderComponent implements OnInit {
 
     declineBaoGia(): void {
         this.modalRef.hide();
+    }
+
+    selectTab(status: string = null) {
+        this.orderService.search.status = status;
+        this.searchOrders();
+    }
+
+    ngOnDestroy() {
+        if (this.sub) {
+            this.sub.unsubscribe();
+        }
     }
 }
