3fb1aa516bdcd7c22e5db31cf746924ebec88b2b
hungcuongqn86@gmail.com <hungcuongqn86@gmail.com>
2019-05-23 22:33:15 +0700

    search package_code

--- src/app/services/order/order.service.ts
+++ src/app/services/order/order.service.ts
@@ -82,7 +82,7 @@ export class OrderService {
     static instance: OrderService;
     private handleError: HandleError;
     private moduleUri = 'order/';
-    public search = {key: '', code: '', status: '', limit: 20, page: 1};
+    public search = {key: '', code: '', package_code: '', status: '', limit: 20, page: 1};
     public order: OrderCreate;
     public orderRe: Order;
 
