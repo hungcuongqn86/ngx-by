bfc8a5c004370b9e338f243150576950d71a5202
hungcuongqn86@gmail.com <hungcuongqn86@gmail.com>
2019-06-01 09:18:06 +0700

    bill

--- src/app/modules/warehouse/bill/bill.detail.component.ts
+++ src/app/modules/warehouse/bill/bill.detail.component.ts
@@ -12,6 +12,7 @@ import {Bill} from '../../../models/Warehouse';
 export class BillDetailComponent implements OnInit {
     bill: Bill = null;
     id: number;
+    date: string;
 
     constructor(private router: Router, private route: ActivatedRoute, public warehouseService: WarehouseService) {
         this.route.params.subscribe(params => {
@@ -26,8 +27,17 @@ export class BillDetailComponent implements OnInit {
             this.warehouseService.getBill(this.id)
                 .subscribe(data => {
                     this.bill = data.data.bill;
+                    console.log(this.bill);
                 });
         }
+        const currentDate = new Date();
+        const day = ('0' + currentDate.getDate()).slice(-2);
+        const month = ('0' + (currentDate.getMonth() + 1)).slice(-2);
+        const year = currentDate.getFullYear();
+        const hours = currentDate.getHours();
+        const minutes = currentDate.getMinutes();
+        const seconds = currentDate.getSeconds();
+        this.date = `${day}-${month}-${year} ${hours}:${minutes}:${seconds}`;
     }
 
     public backlist() {
