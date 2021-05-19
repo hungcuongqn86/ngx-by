794b6e2056cb251c1f965e38883495302aeddf6e
hungcuongqn86 <hungcuongqn86@gmail.com>
2021-05-19 23:40:50 +0700

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -7,16 +7,7 @@ export const environment = {
   basename: '',
   backend: 'http://quangchauonline.com/',
   appId: "dathangqc0",
-  firebase: {
-    apiKey: "AIzaSyBtTPUa19nLkReMDWXTFr6U43D166n9MVE",
-    authDomain: "banhangqc.firebaseapp.com",
-    databaseURL: "https://banhangqc.firebaseio.com",
-    projectId: "banhangqc",
-    storageBucket: "banhangqc.appspot.com",
-    messagingSenderId: "594019390478",
-    appId: "1:594019390478:web:5f43aa461ff0bbd4c0185f",
-    measurementId: "G-D5L6223LZ2"
-  }
+
 };
 
 /*
